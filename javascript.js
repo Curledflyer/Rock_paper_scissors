365ae8d5c71b8820f19a0c7a665e25ccbee4872d
Conor <conor.mciver98@gmail.com>
2023-10-04 14:10:49 +0100

    Added random choice function for computer
    code tested and completed for first section of game cunctionality.
    
    computer can now randomly choose an option based on random number generated.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
-console.log("working");
+/*Part 1, computer randomise choice or Rock, Paper, Scissors. 
+Complete. */
+
+let choice = Math.floor(Math.random() * 100) + 1;
+
+function getComputerChoice() {
+    const    a = 'Rock';
+    const    b = 'Paper';
+    const    c = 'Scissors';
+    
+let result;
+
+    if (choice <= 33) {
+        result = a;
+    } else if (choice > 33 && choice <= 66) {
+        result = b;
+    } else {
+        result = c;
+    }
+    
+   return result
+}
+console.log(getComputerChoice());
