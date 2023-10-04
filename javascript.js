5fda1f6878df5a9f3d2dd29e8df1aad3973a8b57
Conor <conor.mciver98@gmail.com>
2023-10-04 15:21:10 +0100

    added some score keeping
    added the functionality for the console to keep track of who won how many points.
    
    it will also now display a different message depending on if the player or computer won more times.

--- javascript.js
+++ javascript.js
@@ -25,6 +25,9 @@ let computer = getComputerChoice();
 
 /* Part 2:  Function that can take the computer random choice, and the user choice. 
 Based on the choices return a string to declare either the winner or a draw. */
+let computerScore = 0;
+let playerScore = 0;
+
 
 function round() {
 let playerChoice = prompt("Enter your choice of rock, paper, or scissors");
@@ -36,18 +39,22 @@ if ((player == "rock" || player == "Rock") && computer == "Rock") {
 } else if  ((player == "scissors" || player == "Scissors") && computer == "Rock") { 
     console.log(computer);
     console.log("You lose! Rock beats scissors");
+    computerScore = ++computerScore;
 
 } else if  ((player == "scissors" || player == "Scissors") && computer == "Paper") { 
     console.log(computer);
     console.log("You win! Scissor beats paper");
+    playerScore = ++playerScore;
 
 } else if ((player == "Rock" || player == "rock") && computer == "Scissors") {
     console.log(computer);
     console.log("You win! Rock beats scissors");
+    playerScore = ++playerScore;
 
 } else if ((player == "Rock" || player == "rock") && computer == "Paper") {
     console.log(computer);
     console.log("You Lose! Paper beats rock");
+    computerScore = ++computerScore;
 
 } else if  ((player == "scissors" || player == "Scissors") && computer == "Scissors") { 
     console.log(computer);
@@ -60,12 +67,28 @@ if ((player == "rock" || player == "Rock") && computer == "Rock") {
 } else if ((player == "Paper" || player == "paper") && computer == "Scissors") {
     console.log(computer);
     console.log("You Lose! Scissors beat paper");
+    computerScore = ++computerScore;
 
 } else if ((player == "Paper" || player == "paper") && computer == "Rock") {
     console.log(computer);
     console.log("You win! Paper beats rock");
+    playerScore = ++playerScore;
 } else {
     console.log("Try again. please only input either Rock, Paper, or Scissors!");
 }
 }
 alert(round());
+
+/* Part 3: Keep score and display winner after 5 rounds */
+
+
+function gameFive() {
+if (playerScore > computerScore) {
+    console.log(`Congrats! you won ${playerScore} : ${computerScore}.`);
+} else if (computerScore > playerScore) {
+    console.log(`You lose! the score was ${computerScore} : ${playerScore}.`);
+} else {
+    console.log(`It's a draw! The score was ${computerScore} : ${playerScore}`)
+}
+}
+console.log(gameFive());
