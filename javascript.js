9911a3ed6bfb68659bf0f3b349aeffe2dfa74503
Conor <conor.mciver98@gmail.com>
2023-10-04 14:47:01 +0100

    full console functionality added to the game.
    able to now fully play rock paper scissors mainly in console.
    
    added a prompt when the page is loaded for user to enter choice.
    
    Depending on the user choice and the computer random choice a correct response should come out.
    
    fully tested and working in  the console.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-/*Part 1, computer randomise choice or Rock, Paper, Scissors. 
+/*Part 1:   computer randomise choice or Rock, Paper, Scissors. 
 Complete. */
 
 let choice = Math.floor(Math.random() * 100) + 1;
@@ -20,4 +20,51 @@ let result;
     
    return result
 }
-console.log(getComputerChoice());
+let computer = getComputerChoice();
+
+
+/* Part 2:  Function that can take the computer random choice, and the user choice. 
+Based on the choices return a string to declare either the winner or a draw. */
+
+function round() {
+let player = prompt("Enter your choice of rock, paper, or scissors");
+if ((player == "rock" || player == "Rock") && computer == "Rock") {
+    console.log(computer);
+    console.log("It's a draw, both choose rock");
+
+} else if  ((player == "scissors" || player == "Scissors") && computer == "Rock") { 
+    console.log(computer);
+    console.log("You lose! Rock beats scissors");
+
+} else if  ((player == "scissors" || player == "Scissors") && computer == "Paper") { 
+    console.log(computer);
+    console.log("You win! Scissor beats paper");
+
+} else if ((player == "Rock" || player == "rock") && computer == "Scissors") {
+    console.log(computer);
+    console.log("You win! Rock beats scissors");
+
+} else if ((player == "Rock" || player == "rock") && computer == "Paper") {
+    console.log(computer);
+    console.log("You Lose! Paper beats rock");
+
+} else if  ((player == "scissors" || player == "Scissors") && computer == "Scissors") { 
+    console.log(computer);
+    console.log("It's a draw, both choose scissors");
+
+} else if  ((player == "paper" || player == "Paper") && computer == "Paper") { 
+    console.log(computer);
+    console.log("It's a draw, both choose paper");
+
+} else if ((player == "Paper" || player == "paper") && computer == "Scissors") {
+    console.log(computer);
+    console.log("You Lose! Scissors beat paper");
+
+} else if ((player == "Paper" || player == "paper") && computer == "Rock") {
+    console.log(computer);
+    console.log("You win! Paper beats rock");
+} else {
+    console.log("Try again. please only input either Rock, Paper, or Scissors!");
+}
+}
+alert(round());
